a9630d0b393601ff163db8394daea10dec575634
Bram Donovan <bramd@MacBook-Pro.local>
2022-04-27 16:30:03 -0600

    mysterious err

--- 3d-meta/components/layouts/main.js
+++ 3d-meta/components/layouts/main.js
@@ -6,12 +6,13 @@ import Navbar from '../Navbar'
 const Layout = ({ children, router }) => {
     return (
         <>
+        <Box as='main'>
             <Head>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <title>METATECH Digital - Homepage</title>
                 <link rel='shortcut icon' href='/orb.svg' />
             </Head>
-        <Box as='main'>
+            
             <Navbar path={router.asPath} /> 
 
             <Box

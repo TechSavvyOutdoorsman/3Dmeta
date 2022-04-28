d5397c01b79269d9fd79aafbe0a194f27ba4c188
Bram Donovan <bramd@MacBook-Pro.local>
2022-04-27 19:58:32 -0600

    cursed file

--- 3d-meta/components/Layout.js
+++ 3d-meta/components/Layout.js
@@ -1,20 +1,21 @@
 import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
 import Navbar from './Navbar'
 
 
 function Layout({ children, router }) {
     return (
-        <>
+        <main>
             <Head>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <title>METATECH Digital - Homepage</title>
                 <link rel='shortcut icon' href='/orb.svg' />
             </Head>
             <Navbar path={router.asPath} /> 
-            <main>
+            <Box p={0} m={0}>
                 {children}
-            </main>
-        </>
+            </Box>
+        </main>
     )
 }
 

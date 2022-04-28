647907fc97cc262f6462d1b8ef60ea350d21b2d0
Bram Donovan <bramd@MacBook-Pro.local>
2022-04-27 19:35:30 -0600

    almost

--- 3d-meta/next.config.js
+++ 3d-meta/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM()
 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig, withTM()
+module.exports = nextConfig

d2b3148dddc4b58e30b2428ac6670ff9113232da
Bram Donovan <bramd@MacBook-Pro.local>
2022-04-27 19:00:23 -0600

    no styles

--- 3d-meta/lib/theme.js
+++ 3d-meta/lib/theme.js
@@ -1,12 +1,5 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const styles = {
-    global: () => ({
-        body: {
-            bg: '#080705'
-        }
-    })
-}
 
 const components = { 
     Heading: {
@@ -63,7 +56,6 @@ const colorScheme = {
 
 
 const theme = extendTheme({
-    styles,
     components,
     colors, 
     colorScheme,

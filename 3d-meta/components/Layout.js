54e867b0deee2634f1c6a809f27a57e08b6b713f
Bram Donovan <bramd@MacBook-Pro.local>
2022-04-27 19:29:21 -0600

    good job

--- 3d-meta/components/Layout.js
+++ 3d-meta/components/Layout.js
@@ -1,21 +1,21 @@
 import Head from 'next/head'
-import { Box } from '@chakra-ui/react'
+import Box from '@chakra-ui/react'
 import Navbar from './Navbar'
 
 
 function Layout({ children, router }) {
     return (
-        <Box p={0} m={0}>
+        <>
             <Head>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <title>METATECH Digital - Homepage</title>
                 <link rel='shortcut icon' href='/orb.svg' />
             </Head>
-                <Navbar path={router.asPath} /> 
-                <main>
-                    {children}
-                </main>
-        </Box>
+            <Navbar path={router.asPath} /> 
+            <main>
+                {children}
+            </main>
+        </>
     )
 }
 

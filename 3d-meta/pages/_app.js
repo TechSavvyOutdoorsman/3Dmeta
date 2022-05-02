13e1d0377c4598ca05b50ba566d33ad13bea8839
Bram Donovan <bramd@MacBook-Pro.local>
2022-05-02 13:06:28 -0600

    cmon now

--- 3d-meta/pages/_app.js
+++ 3d-meta/pages/_app.js
@@ -1,10 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '../components/layout'
-import Fonts from '../components/fonts' 
+import Layout from '../components/layout.js'
+import Fonts from '../components/fonts.js' 
 import theme from '../lib/theme'
 
 
-function MyApp({ Component, pageProps, router  }) {
+function MyApp({ Component, pageProps, router }) {
   return ( 
     <ChakraProvider theme={theme}>
       <Fonts />

dea7c7a5a39f38ad79fdb2c10b868a8c92a668f0
Bram Donovan <bramd@MacBook-Pro.local>
2022-05-02 17:35:32 -0600

    cute little intro

--- 3d-meta/components/landing/Landing.js
+++ 3d-meta/components/landing/Landing.js
@@ -1,14 +1,38 @@
-
+import { useEffect, useRef } from 'react'
 import {
     Flex,
     Button,
     Heading, 
 } from '@chakra-ui/react'
+import { gsap } from 'gsap'
 import NoSsr from '../No-Ssr'
 import Space from './Space'
+import { useSpring, animated, easings, useSpringRef } from '@react-spring/three'
 
 const Landing = () => {
+    
+    const tl1 = useRef()
+    const heading1 = useRef()
+    const heading2 = useRef()
+    const heading3 = useRef()
 
+    useEffect(() => {
+        let tl1 = new gsap.timeline()
+
+        tl1.from(heading1.current, {
+            opacity: 0,
+            duration: 0.5,
+            delay: 1.5
+        })
+        tl1.from(heading2.current, { 
+            opacity: 0,
+            duration: 0.5, 
+        })
+        tl1.from(heading3.current, { 
+            opacity: 0,
+            duration: 0.5, 
+        })
+    }, [])
 
     return (
         <Flex
@@ -18,10 +42,10 @@ const Landing = () => {
             justify={{ base: 'space-between', md: 'center' }}
             bg='mBlack.900'
             >
-            <Flex  pt={{ base: '8rem', md: '0' }} paddingX={4} position='absolute' zIndex={1} gap={2} flexDir='column' textAlign='center'>
-                <Heading lineHeight='32px' as='h1' color='mWhite.100' fontSize='xx-large'>We Create Unparalled Digital Experiences</Heading>
-                <Heading lineHeight='20px' as='h3' color='mWhite.200' fontSize='large' fontWeight='medium'>Web Applications & Custom Software Solutions That Dominate The Virtual Space.</Heading>
-                <Flex gap={2} pt={2} align='center' justify='center'>
+            <Flex pt={{ base: '8rem', md: '0' }} paddingX={4} position='absolute' zIndex={1} gap={2} flexDir='column' textAlign='center'>
+                <Heading ref={heading1} lineHeight={{ base: '32px', md: '47px' }} as='h1' color='mWhite.100' fontSize={{ base: 'xx-large', md: 'xxx-large'}}>We Create Unparalled Digital Experiences</Heading>
+                <Heading ref={heading2} lineHeight={{ base: '20px', md: '24px' }} as='h3' color='mWhite.200' fontSize={{ base: 'large', md: 'x-large' }} fontWeight='medium'>Web Applications & Custom Software Solutions That Dominate The Virtual Space.</Heading>
+                <Flex ref={heading3} gap={2} pt={2} align='center' justify='center'>
                     <Button variant='solid' bg='meta.500' color='mWhite.100'>Start a Project</Button>                    
                     <Button variant='solid' bg='mWhite.100' color='mBlack.900'>Start a Project</Button>                    
                 </Flex>
--- 3d-meta/pages/index.js
+++ 3d-meta/pages/index.js
@@ -4,11 +4,11 @@ import {
 import Landing from '../components/landing/Landing'
 import LandingTwo from '../components/landing2/Landing'
 
-
 export default function Home() {
   return (
     <Flex
     flexDir='column'
+    id='container'
     >
       <Landing />
       <LandingTwo />

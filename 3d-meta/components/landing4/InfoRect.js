dd007b4045482f7690c9f5643285e28598dae6bd
Bram Donovan <bramd@MacBook-Pro.local>
2022-06-14 15:06:35 -0600

    added color to InfoRect

--- 3d-meta/components/landing4/InfoRect.js
+++ 3d-meta/components/landing4/InfoRect.js
@@ -13,8 +13,8 @@ const InfoRect = ({ heading, text, src, alt }) => {
             <Box mb={3} w='30px' h='30px'>
                 <Image src={src} alt={alt} />
             </Box>
-            <Heading mb={6} as='h3' fontSize='x-large' fontWeight='medium'>{heading}</Heading>
-            <Text as='p' fontSize='medium' fontWeight='light'>{text}</Text>
+            <Heading mb={6} as='h3' color='mWhite.100' fontSize='x-large' fontWeight='medium'>{heading}</Heading>
+            <Text as='p' color='mWhite.100' fontSize='medium' fontWeight='light'>{text}</Text>
         </Flex>
     )
 }

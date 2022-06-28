2036e5349c06c776a6654fb19f2202031596cf14
Bram Donovan <bramd@MacBook-Pro.local>
2022-06-28 16:56:42 -0600

    better mobile spacing

--- 3d-meta/components/landing/Landing.js
+++ 3d-meta/components/landing/Landing.js
@@ -41,18 +41,26 @@ const Landing = () => {
         <Flex
             h={{ base: '90vh', md: '100vh' }}
             direction='column'
-            justify='space-between'
-            paddingX={4}
-            align='center'
+            px={4}
+            pt='5rem'
+            pb='2rem'
             bg='bg.500'
+            sx={{
+                '@media (min-height: 600px)': {
+                    paddingTop: '9rem',
+                    paddingBottom: '5rem'
+                },
+            }}
         >
             <Flex
-                pt={{ base: '5.5rem', md: '13rem' }}
+                // pt={{ base: '5.5rem', md: '13rem' }}
+                paddingX={2}
                 position='absolute'
                 zIndex={1}
                 gap={2}
                 flexDir='column'
                 textAlign='center'
+                align='center'
             >
                 <Heading
                     lineHeight={{ base: '32px', md: '65px' }}
@@ -63,15 +71,17 @@ const Landing = () => {
                 >
                     Custom Websites and Applications
                 </Heading>
-                <Heading
-                    lineHeight={{ base: '20px', md: '24px' }}
-                    as='h3'
-                    color='mBlack.800'
-                    fontSize={{ base: 'large', md: 'x-large' }}
-                    fontWeight='medium'
-                >
-                    We&apos;re Here to Help You Dominate The Virtual Space
-                </Heading>
+                <Box maxW={{ base: '250px', md: '100%'}} >
+                    <Heading
+                        lineHeight={{ base: '20px', md: '24px' }}
+                        as='h3'
+                        color='mBlack.800'
+                        fontSize={{ base: 'large', md: 'x-large' }}
+                        fontWeight='medium'
+                    >
+                        We&apos;re Here to Help You Dominate The Virtual Space
+                    </Heading>
+                </Box>
                 <Flex
                     mt={{ base: '1rem', }}
                     align='center'
@@ -86,7 +96,6 @@ const Landing = () => {
             </Flex>
             <Spacer />
             <Box
-                pb={8}
                 align='center'
                 h={{ base: '40%', md: '60%' }}
             >

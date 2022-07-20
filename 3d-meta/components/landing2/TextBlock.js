a004d81d8062136204b76f1492d8df709c975290
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-07-19 18:11:33 -0600

    added maxW to /components/landing2/TextBlock

--- 3d-meta/components/landing2/TextBlock.js
+++ 3d-meta/components/landing2/TextBlock.js
@@ -9,7 +9,7 @@ const TextBlock = ({ heading, body }) => {
         <Flex
             flexDir='column'
             gap='1rem'
-            w='380px'
+            maxW='container.md'
         >
             <Heading as='h2' color='mBlack.900'>{heading}</Heading>
             <Text as='p' color='mBlack.900' lineHeight='6'>{body}</Text>

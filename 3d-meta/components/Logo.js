c362ba4134ce1d05cd02707b109e735ff9fe8c96
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-07-21 14:42:21 -0600

    nav logo is now priority

--- 3d-meta/components/Logo.js
+++ 3d-meta/components/Logo.js
@@ -25,7 +25,7 @@ const Logo = () => {
         <Link href='/'>
             <a>
                 <LogoBox>
-                    <Image src={navImg} layout='fill' alt='logo' />
+                    <Image src={navImg} layout='fill' alt='logo' priority />
                 </LogoBox>
             </a>
         </Link>

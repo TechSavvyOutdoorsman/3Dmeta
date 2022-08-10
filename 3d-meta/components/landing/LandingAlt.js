9291ae9c2abad3bde6abc0fa998a2b6e43510627
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-08-10 17:58:34 -0600

    fixed landing page for desktop

--- 3d-meta/components/landing/LandingAlt.js
+++ 3d-meta/components/landing/LandingAlt.js
@@ -30,28 +30,31 @@ const LandingAlt = () => {
             flexDir='column'
             bg='bg.500'
             pos='relative'
+            align='center'
         >
-                <BackgroundCanvas  />
+            <BackgroundCanvas />
+
             <Flex
                 flex={1}
-                mb={{ base: '1rem', lg: '3rem'}}
                 gap={{ base: '1rem', md: '2rem', xl: '4rem' }}
-                px={{ base: '1rem', lg: '2rem', xl: '6rem' }}
+                mb={{ base: '1rem', lg: '3rem'}}
+                // px={{ base: '1rem', lg: '2rem', xl: '6rem' }}
                 pb={{ base: '1rem', lg: '2rem', xl: '3rem'}}
+                maxW='container.lg'
                 flexDir='column'
                 textAlign='left'
                 align='left'
                 justify='flex-end'
                 zIndex={1}
-                // maxW={{ base: 'container.md', md: 'container.md', xl: 'container.xl' }}
+                
             >
                 <Box>
                     <Heading
                         as='h1'
                         ref={heading1}
                         // maxW={{ base: '300px', lg: '750px'}}
-                        lineHeight={{ base: '45px', md: '65px', xl: '170px' }}
-                        fontSize={{ base: '45px', md: '64px', xl: '200px' }}
+                        lineHeight={{ base: '45px', md: '65px', xl: '85px' }}
+                        fontSize={{ base: '45px', md: '64px', xl: '90px' }}
                         color='mBlack.900'
                         fontWeight='bold'
                     >

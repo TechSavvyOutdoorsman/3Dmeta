dcb31021e3e70355d2a7f32f1883a78ec0ce9a44
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-08-12 15:09:53 -0600

    removed env for landing 3d element

--- 3d-meta/components/landing/Blob.js
+++ 3d-meta/components/landing/Blob.js
@@ -1,7 +1,7 @@
 import { Box, Spinner } from '@chakra-ui/react'
 import { useRef, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, PerspectiveCamera, Float,  Html, Environment } from '@react-three/drei'
+import { useGLTF, PerspectiveCamera, Float,  Html } from '@react-three/drei'
 import { Box as FlexBox, } from '@react-three/flex'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import useMediaQuery from '../../hooks/useMediaQuery'
@@ -164,7 +164,6 @@ const BackgroundCanvas = ({ props }) => {
             <Canvas>
                 <Suspense fallback={<Loading />}>
                     <spotLight color='#999999' position={[-950, 900, 1500]} angle={120} penumbra={1} intensity={1} />
-                    <Environment preset='night'  />
                     <PerspectiveCamera makeDefault fov={15}>
                         <FlexBox>
                             <MetaSphere position={isDesktop ? [120, -20, -1400] : [0, 0, -1400]} />
